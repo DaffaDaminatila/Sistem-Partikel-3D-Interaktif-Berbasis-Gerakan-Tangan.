6707894d4fc64b66519d2c7c0c6520904297c49d
Muhammad Daffa Daminatila <91118761+DaffaDaminatila@users.noreply.github.com>
2025-12-18 08:15:45 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     // Use relative base path so it works in any subdirectory (like GitHub Pages)
-    base: 'Sistem-Partikel-3D-Interaktif-Berbasis-Gerakan-Tangan.'
+    base: 'Sistem-Partikel-3D-Interaktif-Berbasis-Gerakan-Tangan./'
 });
